edad9c250b446f68b695ea93d5f6c5c997fd289b
polyaks <steve.polyak@act.org>
2020-08-13 15:16:32 -0500

    update

--- Contents.swift
+++ Contents.swift
@@ -20,3 +20,43 @@ print(message)
 
 var name = "Steve Polyak"
 print(name)
+
+/* variables */
+
+var name1 = "bob" //String
+var age = 51 //Integer
+var weight = 212.15 //Double
+var isOrganDonor = false //Boolean
+
+print(weight)
+weight = 200
+print(weight)
+
+let eyeColor = "Blue"
+//eyeColor = "Brown"
+
+/*:
+### Exercise
+
+Consider a car.  What attributes and properties of a car could we capture using variables and constants? Which attributes would be mutable and which would be immutable?
+
+Create variables and constants for the following attributes of a car, and then print each one.
+
+1.  paintColor
+2.  make
+3.  model
+4.  year
+5.  isManualTransmission
+6. seats
+7. mileage
+*/
+
+var paintColor = "red"
+let make = "Chevy"
+let model = "Nova"
+let year = 1969
+let isManualTransmission = true
+let seats = 4
+var mileage = 250000
+print (make)
+print (model)
