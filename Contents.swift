2ce5bef7832e4ddc4f28cbcc87f27e061a7480a7
spolyak <stevepolyak@mac.com>
2020-08-16 13:42:40 -0500

    update

--- Contents.swift
+++ Contents.swift
@@ -268,3 +268,56 @@ for x in (0..<pastPurchases.count).reversed() {
     print(pastPurchases[x])
 }
 
+// dictionaries
+// Swift data collections: Array, Set, Dictionary
+
+var namesOfIntegers = [Int: String]()
+namesOfIntegers[3] = "three"
+namesOfIntegers = [:] // clear out all values
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "LAX": "Los Angeles International"]
+print("Number of airports \(airports.count)")
+if airports.isEmpty {
+    print("No airports")
+}
+airports["LHR"] = "London"
+airports["LHR"] = "London Heathrow"
+airports["Foo"] = nil
+
+for (code, airportTitle) in airports {
+    print("The code, \(code), has value \(airportTitle)")
+}
+for key in airports.keys {
+    print(key)
+}
+for val in airports.values {
+    print(val)
+}
+
+/*:
+### Exercise
+
+Consider a real-world dictionary that you might read on your desk.
+
+1.  Create an animal dictionary where the key is the name of the animal and the value should be a string which represents the definition of the animal
+2.  Add ten animals to this dictionary
+3.  Iterate through the dictionary and print the keys and values in this format: `"Animal: X -- Description: Y"` where X is the name of the animal and Y is the description of the animal
+4.  Clear out the entire dictionary and then instead use this dictionary to store United States state abbrieviations and full names (ie CA : California)
+5. Store ten state names and abbreviations in this dictionary and then print them in this format: `Y: X` where Y is the name of the state and where X is the abbreviation
+6. Set the first state you chose to `nil`
+7. Iterate through the array and print each key and value and see what happened to that state you set to nil
+*/
+
+var dictionary: [String: String] = ["Cat": "Cats are fun", "Dog": "Dogs drool", "Rabbit": "Rabbits run"]
+for (kind, description) in dictionary {
+    print("Animal: \(kind) -- Description: \(description)")
+}
+dictionary = [:]
+dictionary = ["IA": "Iowa", "IL": "Illinois", "NY": "New York"]
+for (code, title) in dictionary {
+    print("\(code): \(title)")
+}
+dictionary["IA"] = nil
+for (code, title) in dictionary {
+    print("\(code): \(title)")
+}
